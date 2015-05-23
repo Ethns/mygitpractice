b4689df2b84b3b5a0eb9a18eb87c02e6e2feb695
EthanShen <shenye601@hotmail.com>
2015-05-23 14:53:43 +0800

    new a branch-A

---
+++ sourcecode-C.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: sourcecode-B.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sat May 23 14:39:31 2015
+ ************************************************************************/
+
+public class sourcecode-C
+
+  private String location;
+
+  public void setLocation(String location){
+      this.location = location;
+  }
