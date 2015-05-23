eb4af68f5bfedd81543a34c57cb4a45ee7bd2047
EthanShen <shenye601@hotmail.com>
2015-05-23 14:41:56 +0800

    add 2 java files

---
+++ sourcecode-A.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: sourcecode-A.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sat May 23 14:37:49 2015
+ ************************************************************************/
+
+public class sourcecode-A
+
+  private String name;
+
+  public String getName(){
+    return this.name;
+  }
---
+++ sourcecode-B.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: sourcecode-B.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sat May 23 14:39:31 2015
+ ************************************************************************/
+
+public class sourcecode-B
+
+  private String desc;
+
+  public void setDesc(String desc){
+      this.desc = desc;
+  }
